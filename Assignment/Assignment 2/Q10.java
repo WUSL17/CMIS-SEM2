66180d51f486e2089a57450e5f3299b3f16a7458
Chamod Shehanka <hcsperera@outlook.com>
2018-10-19 22:12:04 +0530

    all done

--- Assignment/Assignment 2/Q10.java
+++ Assignment/Assignment 2/Q10.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Q10 {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
 
+        System.out.print("Input number : ");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num ; i++) {
+            System.out.println(i * i);
+        }
     }
 }
